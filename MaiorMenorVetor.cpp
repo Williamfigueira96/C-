b84193389e05deb18fa9aaa35b65cf9e71f8c64e
henriquewatanabe <henrique.97wat@gmail.com>
2016-08-23 23:44:14 -0300

    Add files via upload
    erro cometido por mim arrumado era números positivos e negativos

--- MaiorMenorVetor.cpp
+++ MaiorMenorVetor.cpp
@@ -1,21 +1,21 @@
 // Programa que exibe a maior inteira com sinal digitada
-// FATEC - MC - JCB - 10/08/2016 versÃ£o 0.0 
+// FATEC - MC - JCB - 10/08/2016 versão 0.0 
 #include"Meu.h"
 //
 // entry point do programa
 //
 void main(void)
 {
-	int tMaior = INT_MIN,				// inicializa com a menor inteira possÃ­vel
+	int tMaior = INT_MIN,				// inicializa com a menor inteira possível
 		tMenor = INT_MAX,	
-		vetMaior[QUANTIDADE_INTEIRAS],						// vetor para os nÃºmeros positivos
-		vetMenor[QUANTIDADE_INTEIRAS],						// vetor para os nÃºmeros negativos
+		vetMaior[QUANTIDADE_INTEIRAS],						// vetor para os números positivos
+		vetMenor[QUANTIDADE_INTEIRAS],						// vetor para os números negativos
 		vetTodasInteiras[QUANTIDADE_INTEIRAS],				// vetor com todas as quantidades de inteiras digitadas
 		cWork,												// variavel de trabalho para o BubbleSort
 		nValor,							// recebe cada inteira
-			i,							// indice e contador genÃ©rico
-		cSubMaior,						// sub contador para numeros maiores 	
-		cSubMenor,						// sub contador para numeros menores 
+			i,							// indice e contador genérico
+		cSubPositivo,						// sub contador para numeros maiores 	
+		cSubNegativo,						// sub contador para numeros menores 
 		cMaior,							// contador do trono Maior
 		cMenor;							// contador do trono menor
 	bool HouveTroca;					// Verificador de troca do bubblesort
@@ -23,38 +23,44 @@ void main(void)
 
 	setlocale(LC_ALL, "portuguese_brazil");
 	// loop de QUANTIDADE_INTEIRAS vezes
-	cSubMenor = cSubMaior = cMaior = cMenor = 0;
+	cSubNegativo = cSubPositivo = cMaior = cMenor = 0;
 	for(i = 0; i < QUANTIDADE_INTEIRAS; i++) 
 	{
 		cout << "\n\nDigite a inteira com sinal de ordem: " << i+1 << ": " ;
 		cin >> nValor;					// recebe a inteira			
-		if(nValor > tMaior)			// a digitada Ã© maior que a do trono?
+		if(nValor > tMaior)			// a digitada é maior que a do trono?
 		 {
 			tMaior = nValor;			// salva a inteira de maior valor 
-			vetMaior[cSubMaior] = tMaior;	// o vetor indexado pelo contador vetMaior recebe o valor do trono maior
-			cSubMaior++;					// acrescenta mais um ao contador
+			
 		 }
-		if(nValor < tMenor)			// a digitada Ã© menor que a do trono?	 
+		if(nValor < tMenor)			// a digitada é menor que a do trono?	 
 			{
 				tMenor = nValor;			// salva a inteira de maior valor cMenor++;					// acrescenta +1 ao contador cMenor.
-				vetMenor[cSubMenor] = tMenor; // o vetor indexado pelo contador vetMenor recebe o valor do trono menor
-				cSubMenor++;				// acrescenta mais um ao contador 
+				
 			}
 		if(nValor > 0)
-			 cMaior++;
+		{
+			vetMaior[cSubPositivo] = tMaior;	// o vetor indexado pelo contador vetMaior recebe o valor do trono maior
+			cSubPositivo++;					// acrescenta mais um ao contador
+			cMaior++;
+		}
 		else 
+		{
+			vetMenor[cSubNegativo] = tMenor; // o vetor indexado pelo contador vetMenor recebe o valor do trono menor
+			cSubNegativo++;				// acrescenta mais um ao contador 
 			 cMenor++;
+		}
 		vetTodasInteiras[i] = nValor;
 	} // for i
 	// todas as inteiras foram digitadas
 	cout << "\nA maior inteira foi: " << tMaior << "\nA menor inteira foi: " << tMenor << endl << "Quantidade de positivos digitados: " << cMaior
 		<< endl << "Qauntidade de negativos digitados: " << cMenor << endl; 
 	PAUSA;
-	if(cSubMaior != 0)						// existe algum valor de entrada no trono?
+	if(cSubPositivo != 0)						// existe algum valor de entrada no trono?
 	{ // existe!
-		cout << "Lista de todas as maiores inteiras do trono: ";
+		cout << "Lista de todas as positivas inteiras do trono: ";
 	
-		for(int x = 0; x < cSubMaior; x++)
+		for(int x = 0; x < cSubPositivo; x++)
 		{
 		cout << vetMaior[x]	 << "  ";						// exibe todas as maiores inteiras digitadas do trono
 	
@@ -63,10 +69,10 @@ void main(void)
 		PAUSA;
 	}
 
-	if(cSubMenor !=0)						// existe algum valor de entrada no trono?
+	if(cSubNegativo !=0)						// existe algum valor de entrada no trono?
 	{
-		cout << "Lista de todas as menores inteiras no trono: ";
-		for(int y = 0; y < cSubMenor; y++)
+		cout << "Lista de todas as negativas inteiras no trono: ";
+		for(int y = 0; y < cSubNegativo; y++)
 		{
 		cout << vetMenor[y] << "  ";						// exibe todas as menores inteiras digitadas do trono
 		}
@@ -74,7 +80,7 @@ void main(void)
 		PAUSA;
 	}
 
-	cout << "Todas as inteiras digitadas pelo usuÃ¡rio: ";
+	cout << "Todas as inteiras digitadas pelo usuário: ";
 	for(int t = 0; t < QUANTIDADE_INTEIRAS; t++)
 	{
 		cout << vetTodasInteiras[t] << " ";					// exibe todas as inteiras
@@ -82,7 +88,6 @@ void main(void)
 	cout << endl << endl;
 	PAUSA;
 	// Listagem organizada de valores com bubble sort
-	//-----------------------------------------------------------------------------BUBBLESORT
 	do
 	{
 		HouveTroca = false;
@@ -98,7 +103,6 @@ void main(void)
 		
 		} // fim do for
 	} while (HouveTroca == true);
-	//----------------------------------------------------------------------------------- BUBBLESORT
 	// Exibir a listagem com bublesort
 	for(int t = 0; t < QUANTIDADE_INTEIRAS; t++)
 	{
